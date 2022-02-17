14510b5b913a37fb1f34cc7e7bb65b78d7c29e58
TanguyLunais-Robin <tanguy.lunais-robin@hotmail.fr>
2022-02-17 14:13:33 +0100

    Corrcetion (Bug Hunt) - Dashboard

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -8,19 +8,18 @@ import Logout from "./Logout.js"
 export const filteredBills = (data, status) => {
   return (data && data.length) ?
     data.filter(bill => {
+
       let selectCondition
 
       // in jest environment
       if (typeof jest !== 'undefined') {
         selectCondition = (bill.status === status)
-      }
-      /* istanbul ignore next */
-      else {
+      } else {
         // in prod environment
         const userEmail = JSON.parse(localStorage.getItem("user")).email
         selectCondition =
           (bill.status === status) &&
-          ![...USERS_TEST, userEmail].includes(bill.email)
+          [...USERS_TEST, userEmail].includes(bill.email)
       }
 
       return selectCondition
@@ -67,43 +66,47 @@ export const getStatus = (index) => {
   }
 }
 
+let counterHS;
+let counterHE;
+
 export default class {
-  constructor({ document, onNavigate, store, bills, localStorage }) {
+  constructor({ document, onNavigate, firestore, bills, localStorage }) {
     this.document = document
     this.onNavigate = onNavigate
-    this.store = store
+    this.firestore = firestore
     $('#arrow-icon1').click((e) => this.handleShowTickets(e, bills, 1))
     $('#arrow-icon2').click((e) => this.handleShowTickets(e, bills, 2))
     $('#arrow-icon3').click((e) => this.handleShowTickets(e, bills, 3))
+    this.getBillsAllUsers()
     new Logout({ localStorage, onNavigate })
   }
 
   handleClickIconEye = () => {
     const billUrl = $('#icon-eye-d').attr("data-bill-url")
     const imgWidth = Math.floor($('#modaleFileAdmin1').width() * 0.8)
-    $('#modaleFileAdmin1').find(".modal-body").html(`<div style='text-align: center;'><img width=${imgWidth} src=${billUrl} alt="Bill"/></div>`)
+    $('#modaleFileAdmin1').find(".modal-body").html(`<div style='text-align: center;'><img width=${imgWidth} src=${billUrl} /></div>`)
     if (typeof $('#modaleFileAdmin1').modal === 'function') $('#modaleFileAdmin1').modal('show')
   }
 
   handleEditTicket(e, bill, bills) {
-    if (this.counter === undefined || this.id !== bill.id) this.counter = 0
+    if (counterHE === undefined || this.id !== bill.id) counterHE = 0
     if (this.id === undefined || this.id !== bill.id) this.id = bill.id
-    if (this.counter % 2 === 0) {
+    if (counterHE === 0) {
       bills.forEach(b => {
         $(`#open-bill${b.id}`).css({ background: '#0D5AE5' })
       })
       $(`#open-bill${bill.id}`).css({ background: '#2A2B35' })
       $('.dashboard-right-container div').html(DashboardFormUI(bill))
       $('.vertical-navbar').css({ height: '150vh' })
-      this.counter ++
+      counterHE = 1;
     } else {
       $(`#open-bill${bill.id}`).css({ background: '#0D5AE5' })
 
       $('.dashboard-right-container div').html(`
-        <div id="big-billed-icon" data-testid="big-billed-icon"> ${BigBilledIcon} </div>
+        <div id="big-billed-icon"> ${BigBilledIcon} </div>
       `)
       $('.vertical-navbar').css({ height: '120vh' })
-      this.counter ++
+      counterHE = 0;
     }
     $('#icon-eye-d').click(this.handleClickIconEye)
     $('#btn-accept-bill').click((e) => this.handleAcceptSubmit(e, bill))
@@ -131,56 +134,59 @@ export default class {
   }
 
   handleShowTickets(e, bills, index) {
-    if (this.counter === undefined || this.index !== index) this.counter = 0
+    if (counterHS === undefined || this.index !== index) counterHS = 0
     if (this.index === undefined || this.index !== index) this.index = index
-    if (this.counter % 2 === 0) {
+
+    if ($(e.currentTarget).css("transform") !== "matrix(6.12323e-17, 1, -1, 6.12323e-17, 0, 0)") counterHS = 1
+
+    if (counterHS === 0) {
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)'})
       $(`#status-bills-container${this.index}`)
         .html(cards(filteredBills(bills, getStatus(this.index))))
-      this.counter ++
+      counterHS = 1
     } else {
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)'})
       $(`#status-bills-container${this.index}`)
         .html("")
-      this.counter ++
+      counterHS = 0
     }
-
+ 
     bills.forEach(bill => {
-      $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
+      $(`#open-bill${bill.id}`).click((e) => {
+        counterHE = 0;
+        this.handleEditTicket(e, bill, bills);
+      })
     })
-
+    
     return bills
-
-  }
-
-  getBillsAllUsers = () => {
-    if (this.store) {
-      return this.store
-      .bills()
-      .list()
-      .then(snapshot => {
-        const bills = snapshot
-        .map(doc => ({
-          id: doc.id,
-          ...doc,
-          date: doc.date,
-          status: doc.status
-        }))
-        return bills
-      })
-      .catch(error => {
-        throw error;
-      })
-    }
   }
 
   // not need to cover this function by tests
-  /* istanbul ignore next */
-  updateBill = (bill) => {
-    if (this.store) {
-    return this.store
+  getBillsAllUsers = () => {
+    if (this.firestore) {
+      return this.firestore
       .bills()
-      .update({data: JSON.stringify(bill), selector: bill.id})
+      .get()
+      .then(snapshot => {
+        const bills = snapshot.docs
+        .map(doc => ({
+          id: doc.id,
+          ...doc.data(),
+          date: doc.data().date,
+          status: doc.data().status
+        }))
+        return bills
+      })
+      .catch(console.log)
+    }
+  }
+    
+  // not need to cover this function by tests
+  updateBill = (bill) => {
+    if (this.firestore) {
+    return this.firestore
+      .bill(bill.id)
+      .update(bill)
       .then(bill => bill)
       .catch(console.log)
     }
